af8dcfbbaa188ac80b86e0b7f30c762befc5ff3e
pro100ton <pro100ton@gmail.com>
2023-03-05 13:26:20 +0300

    Add basic layout for app

--- MusicWireframe/ViewController.swift
+++ MusicWireframe/ViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Outlets
+    
+    @IBOutlet var albumImageView: UIImageView!
+    @IBOutlet var reverseButton: UIButton!
+    @IBOutlet var pauseButton: UIButton!
+    @IBOutlet var forwardButton: UIButton!
+    @IBOutlet var reverseBackground: UIView!
+    @IBOutlet var pauseBackground: UIView!
+    @IBOutlet var forwardBackground: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
